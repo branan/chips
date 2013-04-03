b9c524b67a0e28800255dfc30d821904dcc5ee4f
Branan Purvine-Riley <branan@puppetlabs.com>
2013-04-02 19:10:08 -0700

    Rename first-match to idx-first-match for ++descriptivity
    While we're at it, refactor to use keep-indexed. It's a much simpler implementation now

--- src/chips/game.clj
+++ src/chips/game.clj
@@ -10,8 +10,8 @@
     (assoc state
       :world (map build-cell top bot))))
 
-(defn first-match [pred col]
-  (first (map first (filter pred (map-indexed vector col)))))
+(defn idx-first-match [pred col]
+  (first (keep-indexed #(if (pred %2) %1) col)))
 
 (defn player-in-cell? [cell]
   (let [player-ids [108 109 110 111]]
@@ -19,7 +19,7 @@
         (some #(= (:bot cell) %) player-ids))))
 
 (defn locate-player [state]
-  (let [player-pos (first-match #(player-in-cell? (second %)) (:world state))]
+  (let [player-pos (idx-first-match #(player-in-cell? %) (:world state))]
     (-> state
         (assoc :player-y (quot player-pos 32))
         (assoc :player-x (mod player-pos 32)))))
